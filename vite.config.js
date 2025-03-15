8e3ac9f4674cb31951b3d8f40fd7a0a5c808c0ac
mohammedfalaah <falahmkba@gmail.com>
2025-03-15 14:26:00 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/leadersacademy/", // Ensure this matches your repo name
   plugins: [react()],
-});
+})
